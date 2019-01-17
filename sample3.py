f1ea17e989be10aa54fdaeb3afc24bc8d066cbba
li-ten <37286249+tsephel24@users.noreply.github.com>
2019-01-17 15:37:19 +0530

    Update sample3.py

--- sample3.py
+++ sample3.py
@@ -2,3 +2,12 @@ x = int(input("Enter first number:"))
 y = int(input("Enter second number:"))
 sum = x + y
 print("Sum of the given two numbers is:", sum)
+
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+sum = x + y
+sub = x - y
+print("Sum of the given two numbers is:", sum)
+print("Sub of the given two numbers is:", sub)
+
+
